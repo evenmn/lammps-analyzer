259e93d2128485b557b1b28cf0a933872e0da6da
evenmn <evenmn@mn.uio.no>
2024-04-23 20:58:52 +0200

    Update

--- lammps_analyzer/chunkavg.py
+++ lammps_analyzer/chunkavg.py
@@ -3,59 +3,60 @@
 
 import numpy as np
 
+
 class ChunkAvg:
-    
+
     def __init__(self, filename):
         self.read_data(filename)
-        
+
     def read_data(self, filename):
-        """Read chunk-averaged file. 
-        
+        """Read chunk-averaged file.
+
         Assumptions:
-        The file starts with three comment lines. The first line is some 
-        general information. The second line contains global information. 
-        The third line contains local information. 
+        The file starts with three commented lines. The first line contains
+        some general information. The second line contains global information.
+        The third line contains local information.
         """
-        
+
         with open(filename, "r") as f:
-        
+
             line1 = f.readline()
             line2 = f.readline()
             line3 = f.readline()
-            
+
             self.global_labels = line2.split()[1:]
             self.local_labels = line3.split()[1:]
-            
+
             global_list = []
             local_list = []
-            
-            for line in f.readlines():
+
+            for line in f:
                 splitted = line.split()
                 if line[0].isdigit():
                     # global
                     global_list.append(splitted)
                     local_list.append([])
-                    
+
                 elif line.startswith(" "):
                     # local
                     local_list[-1].append(splitted)
-                    
+
                 else:
                     raise TypeError("???")
-            
-        self.global_list = np.array(global_list, dtype=float).transpose(1,0)
-        self.local_list = [np.array(i, dtype=float).transpose(1,0) for i in local_list]
-        
+
+        self.global_list = np.array(global_list, dtype=float).transpose(1, 0)
+        self.local_list = [np.array(i, dtype=float).transpose(1, 0) for i in local_list]
+
     def global_labels(self):
         """Get an overview of all the global labels
         """
         return self.global_labels
-        
+
     def local_labels(self):
         """Get an overview of all the local labels
         """
         return self.local_labels
-        
+
     def find_global(self, key):
         """Returns the ...
         """
@@ -67,7 +68,7 @@ class ChunkAvg:
             raise KeyError("No category named {} found.".format(key))
         else:
             return np.array(array)
-        
+
     def find_local(self, key, step=0):
         """Returns the ...
         """
@@ -79,57 +80,57 @@ class ChunkAvg:
             raise KeyError("No category named {} found.".format(key))
         else:
             return np.array(array)
-            
+
     def find_crack_tip(self, step, threshold, window, ignore_last):
-        """Cracktip at time step 
+        """Cracktip at time step
         """
         from . import pooling1d
-        
+
         pad_size = (window - 1) // 2
-        
+
         positions = self.find_local("Coord1", step=step)[:-ignore_last]
         edgeatoms = self.find_local("v_edgeatom", step=step)[:-ignore_last]
         edgeatoms = pooling1d(edgeatoms, window=window, pad_size=pad_size, mode='min')
-        
+
         # Find crack tip
-        crackbin = (edgeatoms>threshold).nonzero()[0][-1]
+        crackbin = (edgeatoms > threshold).nonzero()[0][-1]
         cracktip = positions[crackbin]
         return cracktip
-            
+
     def find_crack_tips(self, threshold, window, ignore_last=1):
         """Determine the crack tip position and speed
         using the number of edge atoms and a threshold.
         """
-        
+
         self.timesteps = self.find_global("Timestep")
         self.cracktip = np.zeros_like(self.timesteps)
-        
+
         for i in range(len(self.timesteps)):
             self.cracktip[i] = self.find_crack_tip(i, threshold, window, ignore_last)
-        
+
         return self.cracktip
-        
+
     def estimate_crack_speed(self, ignore_first=0, ignore_last=0, length_threshold=0.95):
         """Estimate average crack speed. If the crack propagates through
         the entire sample, the end time is when the crack has reached the
         end of the sample. The speed is returned in units Å/timestep
         """
-        
-        end_index = len(self.timesteps) - 1 #ignore_last
+
+        end_index = len(self.timesteps) - 1  # ignore_last
         positions = self.find_local("Coord1", step=0)
         length_sample = positions[len(positions) - ignore_last - 1]
-            
+
         #try:
         #    g = self.cracktip[0]
         #else:
         #    raise ValueError("find_crack_tips needs to be run before this function")
-            
+
         start_time = self.timesteps[ignore_first]
         end_time = self.timesteps[end_index]
-        
+
         start_pos = self.cracktip[ignore_first]
         end_pos = self.cracktip[end_index]
-        
+
         end = length_sample * length_threshold
 
         if end_pos > end:
@@ -138,24 +139,22 @@ class ChunkAvg:
 
         speed = (end_pos - start_pos) / (end_time - start_time)
         return speed
-        
 
     def plot_edge_fraction(self, step=0, show=False, save=False, ignore_first=3, ignore_last=10, threshold=0.004, window=1):
-        """Given a chunk average file with the number of atoms of coordination number 1, 
+        """Given a chunk average file with the number of atoms of coordination number 1,
         this function plots the edge fraction at a given timestep.
         """
         edgeatoms = self.find_local("v_edgeatom", step=step)
         positions = self.find_local("Coord1", step=step)
         timesteps = self.find_global("Timestep")
         current_timestep = int(timesteps[step])
-        
+
         cracktip = self.find_crack_tip(step, threshold, window, ignore_last)
 
         import matplotlib.pyplot as plt
 
-        label_size = {"size":14}                # Dictionary with size
-        #plt.style.use("bmh")                    # Beautiful plots
-        plt.rcParams["font.family"] = "Serif"   # Font
+        label_size = {"size": 14}                # Dictionary with size
+        plt.rcParams["font.family"] = "Serif"    # Font
         plt.rcParams.update({'figure.max_open_warning': 0})
         plt.figure()
         plt.plot(positions[ignore_first:-ignore_last], edgeatoms[ignore_first:-ignore_last])
@@ -167,3 +166,100 @@ class ChunkAvg:
             plt.savefig(save + f"edge_fraction_{current_timestep}.png")
         if show is not False:
             plt.show()
+
+
+class ChunkAvgMemSave:
+
+    def __init__(self, filename):
+        self.read_data(filename)
+
+    def read_data(self, filename):
+        """Read chunk-averaged file.
+
+        Assumptions:
+        The file starts with three comment lines. The first line is some
+        general information. The second line contains global information.
+        The third line contains local information.
+        """
+        print("Reading headers")
+        import subprocess
+
+        self.global_labels = subprocess.getstatusoutput(f"sed '2q;d' {filename}")[1].split()[1:]
+        self.local_labels = subprocess.getstatusoutput(f"sed '3q;d' {filename}")[1].split()[1:]
+
+
+        first_globals = subprocess.getstatusoutput(f"sed '4q;d' {filename}")[1].split()
+        start_timestep = int(first_globals[0])
+        n_chunks = int(first_globals[1])
+
+        next_global = 4 + n_chunks + 1
+        second_timestep = int(subprocess.getstatusoutput(f"sed '{next_global}q;d' {filename}")[1].split()[0])
+
+        dt = second_timestep - start_timestep
+
+        last_global = n_chunks + 1
+        last_timestep = int(subprocess.getstatusoutput(f"tail -n {last_global} {filename}| head -n 1")[1].split()[0])
+
+        number_of_timesteps = (last_timestep - start_timestep)//dt + 1
+
+        #allocate arrays:
+        global_array = np.zeros((number_of_timesteps,len(self.global_labels)),dtype=float)
+        local_array = np.zeros((number_of_timesteps,len(self.local_labels),n_chunks),dtype=float)
+
+        print("Reading file: ")
+        with open(filename, "r") as f:
+            timestep_index = -1
+            chunk_nr = 0
+            for line in f:
+                splitted = line.split()
+                if line[0].isdigit():
+                    # global
+                    chunk_nr = 0
+                    timestep_index += 1
+                    global_array[timestep_index] = np.asarray(splitted, dtype=float)
+
+                elif line.startswith(" "):
+                    # local
+                    local_array[timestep_index, :, chunk_nr] = np.asarray(splitted, dtype=float)
+                    chunk_nr += 1
+
+                else:
+                    raise TypeError("???")
+
+        self.global_list = global_array
+        self.local_list = local_array
+        print("File read")
+
+    def global_labels(self):
+        """Get an overview of all the global labels
+        """
+        return self.global_labels
+
+    def local_labels(self):
+        """Get an overview of all the local labels
+        """
+        return self.local_labels
+
+    def find_global(self, key):
+        """Returns the ...
+        """
+        array = None
+        for i, variable in enumerate(self.global_labels):
+            if variable == key:
+                array = self.global_list[:,i]
+        if array is None:
+            raise KeyError("No category named {} found.".format(key))
+        else:
+            return np.array(array)
+
+    def find_local(self, key, step=0):
+        """Returns the ...
+        """
+        array = None
+        for i, variable in enumerate(self.local_labels):
+            if variable == key:
+                array = self.local_list[step,i,:]
+        if array is None:
+            raise KeyError("No category named {} found.".format(key))
+        else:
+            return np.array(array)
---
+++ lammps_analyzer/stressfield.py
@@ -0,0 +1,293 @@
+from lammps_analyzer.chunkavg import ChunkAvg,ChunkAvgMemSave
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class Stressfield():
+
+    """
+    Class for reading chunkavg files of the stressfield sigma_ij from LAMMPS.
+    """
+
+    def __init__(self, filename, dt=0.001, loc=".", loadfromnpy=False, xlength=950, ylength=121.5, ntimesteps=50000):
+        if loadfromnpy:
+            self.loc = loc
+            self.xlength = xlength
+            self.ylength = ylength
+            self.ntimesteps = ntimesteps
+            self.load_from_npy()
+            print("Loaded from .npy: beware some methods are not implemented for this initialization.")
+        else:
+            self.data = ChunkAvgMemSave(filename)
+        self.component_ordering = {"xx": 1, "yy": 2, "zz": 3, "xy": 4}
+        self.loadfromnpy = loadfromnpy
+        self.dt = dt
+
+    def _get_axes(self, step=0):
+        coordx = self.data.find_local("Coord1", step=step)
+        coordy = self.data.find_local("Coord2", step=step)
+
+        # Count how many bins in each dim
+        i = np.size(np.unique(coordx))
+        j = np.size(np.unique(coordy))
+
+        assert i*j == self.data.find_global("Number-of-chunks")[step], "Error in counting bins per axis"
+
+        mesh_XX = np.resize(coordx, (i, j))
+        mesh_YY = np.resize(coordy, (i, j))
+
+        nbins_X = i
+        nbins_Y = j
+        return mesh_XX, mesh_YY, nbins_X, nbins_Y
+
+    def create_stressfield(self, step=0, component="xx"):
+
+        if not self.loadfromnpy:
+            mesh_XX, mesh_YY, nbins_X, nbins_Y = self._get_axes(step=step)
+            component_index = self.component_ordering[component]
+
+            stressfield = self.data.find_local(f"c_stressperatom[{str(component_index)}]", step=step)
+            stressfield = np.resize(stressfield, (nbins_X, nbins_Y))
+
+            self.stress = (mesh_XX, mesh_YY, stressfield, nbins_X, nbins_Y, component,step)
+        elif self.loadfromnpy:
+            mesh_XX, mesh_YY, timesteps, stressfields = self.load_from_npy(component=component)
+            stressfield = stressfields[step]
+            self.stress = (mesh_XX, mesh_YY, stressfield, mesh_XX[:, 0].size, mesh_YY[0, :].size, component, step)
+        return stressfield
+
+    def create_stressfields(self, component="xx"):
+        if not self.loadfromnpy:
+            self.mesh_XX, self.mesh_YY, nbins_X, nbins_Y = self._get_axes(step=0)
+
+            self.timesteps = self.data.find_global("Timestep")
+            self.stressfields = np.zeros((self.timesteps.size, nbins_X, nbins_Y))
+
+            for t in range(self.timesteps.size):
+                self.stressfields[t] = self.create_stressfield(step=t, component=component)
+
+            self.last_component_created = component
+            return self.mesh_XX, self.mesh_YY, self.timesteps, self.stressfields
+        else:
+            return self.load_from_npy(component=component)
+
+    def save_stressfields(self,loc="."):
+        print("Saving.",end="")
+        self.create_stressfields(component="xx")
+        np.save(loc+"/stress_xx.npy",self.stressfields)
+        print(".",end="")
+        self.create_stressfields(component="xy")
+        np.save(loc+"/stress_xy.npy",self.stressfields)
+        print(".",end="\n")
+        self.create_stressfields(component="yy")
+        np.save(loc+"/stress_yy.npy",self.stressfields)
+        print(f"Done! Saved to : {loc}/stress_ij.npy")
+
+    def load_from_npy(self,component="xy"):
+        self.stressfields = np.load(self.loc + f"/stress_{component}.npy")
+        self.last_component_created=component
+
+        x = np.linspace(0,self.xlength,self.stressfields.shape[1])
+        y = np.linspace(0,self.ylength,self.stressfields.shape[2])
+        self.mesh_XX,self.mesh_YY = np.meshgrid(x,y)
+        self.timesteps = np.linspace(0,self.ntimesteps,self.stressfields.shape[0])
+
+        return self.mesh_XX,self.mesh_YY,self.timesteps, self.stressfields
+
+    def average_window(self,window_time,at_step=0,component="xy"):
+
+        if not hasattr(self,"last_component_created") and self.loadfromnpy:
+            self.create_stressfields(component=component)
+        elif self.last_component_created != component and not self.loadfromnpy:
+            self.create_stressfields(component=component)
+        elif self.last_component_created != component and self.loadfromnpy:
+            self.load_from_npy(component=component)
+        return np.average(self.stressfields[at_step:at_step+window_time,:,:],axis=0)
+
+    def average_windows(self,window_time,component="xy"):
+        if not hasattr(self,"last_component_created") and not self.loadfromnpy:
+            self.create_stressfields(component=component)
+        elif self.last_component_created != component and not self.loadfromnpy:
+            self.create_stressfields(component=component)
+        elif self.last_component_created != component and self.loadfromnpy:
+            self.load_from_npy(component=component)
+
+        t,x,y = self.stressfields.shape
+        self.avg_stressfield = np.zeros((t-window_time,x,y))
+
+
+        for i in range(t-window_time):
+            self.avg_stressfield[i,:,:] = self.average_window(window_time,at_step=i,component=component)
+        return self.avg_stressfield
+
+    def animate(self,window_time,component="xy", cm="bwr",save=None,dpi=100):
+        from matplotlib.animation import FuncAnimation
+
+        frames = self.average_windows(window_time = window_time,component=component)
+
+        vmax = np.max(np.abs(frames))
+        vmin = -vmax
+
+        plt.clf()
+        fig = plt.figure()
+        im = plt.imshow(frames[0].T,cmap=cm,vmin=vmin,vmax=vmax,extent=[self.mesh_XX[0,0],self.mesh_XX[0,-1],self.mesh_YY[0,0],self.mesh_YY[-1,0]])#aspect="equal",interpolation="none")
+        ax = plt.gca()
+
+        def animate_function(i):
+            im.set_array(frames[i].T)
+            ax.set_title(f"$\\sigma_{{{component}}}$ at t = {i/frames.shape[0]*self.timesteps[-1]*self.dt:.2f} ps")
+            return [im]
+
+        anim = FuncAnimation(fig,animate_function,frames=frames.shape[0],interval=100)
+        if save:
+            anim.save(save, dpi=dpi,extra_args=['-vcodec', 'libx264'])
+        plt.show()
+
+    def contourplot_stressfield(self,logplot=False,stress=None,show=False,save=False):
+        """
+        If you want to save, save should be the string of the path/filename of desired output.
+        If stress is None, this plots the stressfield made by the previous create_stressfield call.
+        """
+        if stress is None and not self.loadfromnpy:
+            assert hasattr(self,"stress"), "create_stressfield must be called first or stress field passed as arg!"
+            stress = self.stress
+
+        mesh_XX,mesh_YY,stressfield,nbins_X,nbins_Y,component,step = stress
+
+        if logplot:
+            stressfield=np.where(stressfield!=0,np.log(np.abs(stressfield)),0)
+            cbartitle = "stress field value [log]"
+        else:
+            cbartitle = "stress field value"
+
+        plt.contourf(mesh_XX,mesh_YY,stressfield)
+        plt.title(f"Stress field $\\sigma_{{{component}}}$ nbins = {nbins_X*nbins_Y} at timestep = {step}")
+        plt.xlabel("x-length [Å]")
+        plt.ylabel("y-length [Å]")
+        cbar  = plt.colorbar()
+        cbar.set_label(cbartitle)
+
+        if save:
+            plt.savefig(save)
+        if show:
+            plt.show()
+
+    def imshow_stressfield(self,logplot=False,stress=None,show=False,save=False,cmap="bwr"):
+        """
+        If you want to save, save should be the string of the path/filename of desired output.
+        If stress is None, this plots the stressfield made by the previous create_stressfield call.
+        """
+        if stress is None:
+            assert hasattr(self,"stress"), "create_stressfield must be called first or stress field passed as arg!"
+            stress = self.stress
+
+        mesh_XX,mesh_YY,stressfield,nbins_X,nbins_Y,component,step = stress
+
+        if logplot:
+            stressfield=np.where(stressfield!=0,np.log(np.abs(stressfield)),0)
+            cbartitle = "stress field value [log]"
+        else:
+            cbartitle = "stress field value"
+
+        vmin,vmax = -np.max(np.abs(stressfield)),np.max(np.abs(stressfield))
+
+        plt.imshow(stressfield.T,cmap=cmap,origin="lower",vmin=vmin, vmax=vmax, extent=[np.min(mesh_XX),np.max(mesh_XX),np.min(mesh_YY),np.max(mesh_YY)])
+        plt.title(f"Stress field $\\sigma_{{{component}}}$ nbins = {nbins_X*nbins_Y} at timestep = {step}")
+        plt.xlabel("x-length [Å]")
+        plt.ylabel("y-length [Å]")
+        cbar = plt.colorbar()
+        cbar.set_label("value of stress field [pressure*volume]")
+        if save:
+            plt.savefig(save)
+        if show:
+            plt.show()
+        plt.clf()
+
+    def avg_tip(self,edge_density,window_time=None,window_space=10,component="xy",cnkargs=(0.004,1,20)):
+        """
+        Create the average around the cracktip
+        edge_density: needs to be an instance of ChunkAvg for finding the cracktip
+        window_time: tuple of two timesteps of where to start and end the average
+        window_space: the resulting average around the cracktip is this value long in each direction
+        component: which component of the stress tensor to average
+        """
+
+        cracktip_positions = edge_density.find_crack_tips(cnkargs[0],cnkargs[1],ignore_last=cnkargs[2])
+        cracktip_timesteps = edge_density.find_global("Timestep")
+
+
+        mesh_XX, mesh_YY, stress_timesteps, stressfields = self.create_stressfields(component=component)
+        if window_time is None:
+            window_time = (np.min(stress_timesteps),np.max(stress_timesteps))
+
+        xaxis = mesh_XX[:,0]
+
+        if cracktip_timesteps.size > stress_timesteps.size:
+            factor = int(cracktip_timesteps.size // stress_timesteps.size)
+            cracktip_positions = cracktip_positions[::factor]
+            cracktip_timesteps = cracktip_timesteps[::factor]
+            indexoffset = int(np.where(cracktip_timesteps==stress_timesteps[0])[0])
+
+        tindices = np.argwhere(np.logical_and(cracktip_timesteps>window_time[0] , cracktip_timesteps<window_time[1]))
+        stress_avg = np.zeros((2*window_space,mesh_YY[0,:].size))
+
+        for tindex in tindices:
+            xindice = np.argmin(np.abs(xaxis-cracktip_positions[tindex]))
+            stress_avg += stressfields[tindex[0]-indexoffset,(xindice-window_space):(xindice+window_space),:]
+
+        return stress_avg/tindices.size
+
+
+
+def _save(direc):
+    print(direc)
+    stress = Stressfield(direc+"/stressfield.data")
+    stress.save_stressfields(loc=direc)
+
+def save_parallel(directory,nproc=8):
+    import os
+    import multiprocessing as mp
+
+    dirs = [ f.name for f in os.scandir(directory) if f.is_dir() ]
+
+    pool = mp.Pool(nproc)
+    pool.map(_save,dirs)
+
+
+
+def running_average(array,N):
+    i,*dims = array.shape
+
+    avg = np.zeros((i-N,*dims))
+    for indx in range(i-N):
+        avg[indx] = np.average(array[indx:indx+N],axis=0)
+    return avg
+
+
+def alternative_animate(frames,N,title,axis_dims=None, cm="bwr",save=None,dpi=100):
+        from matplotlib.animation import FuncAnimation
+
+        frames = running_average(frames,N)
+
+        vmax = np.max(np.abs(frames))
+        vmin = -vmax
+
+        if axis_dims is not None:
+            x0,x1,y0,y1 = axis_dims
+        else:
+            x0,x1,y0,y1 = 0,frames.shape[1],0,frames.shape[2]
+
+        plt.clf()
+        fig = plt.figure()
+        im = plt.imshow(frames[0].T,cmap=cm,vmin=vmin,vmax=vmax,extent=[x0,x1,y0,y1])#aspect="equal",interpolation="none")
+        ax = plt.gca()
+
+        def animate_function(i):
+            im.set_array(frames[i].T)
+            ax.set_title(f"{title} frame {i}")
+            return [im]
+
+        anim = FuncAnimation(fig,animate_function,frames=frames.shape[0],interval=100)
+        if save:
+            anim.save(save, dpi=dpi,extra_args=['-vcodec', 'libx264'])
+        plt.show()
